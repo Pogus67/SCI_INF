606e200cf8558a44059733aac052383a7029fa26
Pogus <54739180+Pogus67@users.noreply.github.com>
2021-11-08 21:08:10 +0100

    chyba ostatni

--- PracaNaLekcjiNr1/ConsoleApplication12.cpp
+++ PracaNaLekcjiNr1/ConsoleApplication12.cpp
@@ -9,48 +9,123 @@ string zmienna;
 int liczba;
 char wybranie;
 
-string szyfrcezara(string a, int b) {
-	for (int i = 0; i <= a.size(); i++) {
-		if ((a[i] + b) >= 97 and (a[i] + b) <= 122) 
-		{
-			a[i] = int(a[i]) + b;
-		}
-		else if ((a[i] + b) > 122)
-		{
-			a[i] = a[i] % 123 + 97;
-		}
+string szyfrcezara(string a) {
+	int b;
+	cout << "O ile chcesz przesunac cyfry? (1-26): ";
+	cin >> b;
+	string c;
+	if (b >= 1 and b <= 26)
+	{
+		for (int i = 0; i < a.size(); i++) {
+			unsigned char d;
+			if (a[i] + b >= 123)
+			{
+				d = ((a[i] + b) % 123) + 97;
+				c += d;
+			}
+			else if (a[i] > 96 && a[i] < 123)
+			{
+				d = a[i] + b;
+				c += d;
+			}
+			else
+			{
+				c += a[i];
+			}
 
+		}
 	}
+	else
+	{
+		cout << "Zla Liczba!" << endl;
+	}
+	return c;
+}
+
+string deszyfrcezara(string a) {
+	int b;
+	cout << "O ile chcesz przesunac cyfry? (1-26): ";
+	cin >> b;
+	string c;
+	if (b >= 1 and b <= 26)
+	{
+		for (int i = 0; i < a.size(); i++) {
+			unsigned char d;
+			if ((a[i] - b) < 96)
+			{
+				d = a[i] - b;
+				char e = (a[i] % 97) + 123;
+				e -= b;
+				d = e;
+				c += d;
+			}
+			else if (a[i] >= 96)
+			{
+				d = a[i] - b;
+				c += d;
+			}
+			else
+			{
+				c += a[i];
+			}
+
+		}
+	}
+	else
+	{
+		cout << "Zla Liczba!" << endl;
+	}
+	return c;
+
+}
+
+string szyfrprzest(string a)
+{
+	string b;
+	for (int i = 0; i <= a.size() - 1; i += 2)
+	{
+		swap(a[i], a[i + 1]);
+		b += a[i];
+		b += a[i + 1];
+	}
+	if (a.size() % 2 == 1)
+	{
+		b += a[a.size() - 1];
+	}
+	return b;
+}
+
+string szyfroba(string a)
+{
+	a = szyfrprzest(szyfrcezara(a));
 	return a;
 }
 
 int main()
-{    
+{
 	cout << "Podaj ciag znakow: ";
 	getline(cin, zmienna);
 	cout << "Jakim sposobem chcesz to zaszyfrowac? (wpisz liczbe)" << endl;
 	cout << "1. Szyfr Cezara" << endl;
 	cout << "2. Szyfr Przestawieniowy" << endl;
-	cout << "2. Szyfr Cezara i Przestawieniowy" << endl;
+	cout << "3. Szyfr Cezara i Przestawieniowy" << endl;
 	cout << "4. Odszyfrowac" << endl;
 	cin >> wybranie;
 	if (wybranie == '1')
 	{
-		cout << "O ile chcesz przesunac cyfry?: ";
-		cin >> liczba;
-		cout << szyfrcezara(zmienna, liczba) << endl;
-	} 
+		cout << "Zaszyfrowany ciag znakow: " << szyfrcezara(zmienna) << endl;
+	}
 	else if (wybranie == '2')
 	{
-
+		cout << "Zaszyfrowany ciag znakow: " << szyfrprzest(zmienna) << endl;
 	}
 	else if (wybranie == '3')
 	{
-
+		cout << "Zaszyfrowany ciag znakow: " << szyfroba(zmienna) << endl;
 	}
 	else if (wybranie == '4')
 	{
-
+		cout << "Zaszyfrowany ciag znakow: " << deszyfrcezara(zmienna) << endl;
 	}
 }
 
